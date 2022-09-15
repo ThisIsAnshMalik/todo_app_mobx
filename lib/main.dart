b3c2fda164fbd8d08d1ad4b0ff045f0f77dcb9b6
Ansh Malik <anshmalik279@gmail.com>
2022-09-15 10:22:51 +0530

    UI is Created

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +41,54 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: const <Widget>[],
+          children: <Widget>[
+            TextField(
+              decoration: const InputDecoration(
+                  labelText: 'Add a Todo', contentPadding: EdgeInsets.all(10)),
+              controller: _textController,
+              textInputAction: TextInputAction.done,
+              onSubmitted: (String value) {},
+            ),
+            RadioListTile(
+                dense: true,
+                title: const Text('All'),
+                value: 1,
+                groupValue: 2,
+                onChanged: (filter) {}),
+            RadioListTile(
+                dense: true,
+                title: const Text('Processing'),
+                value: 2,
+                groupValue: 2,
+                onChanged: (filter) {}),
+            RadioListTile(
+                dense: true,
+                title: const Text('Completed'),
+                value: 3,
+                groupValue: 2,
+                onChanged: (filter) {}),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonBar(
+                  children: <Widget>[
+                    ElevatedButton(
+                        onPressed: (() {}),
+                        child: const Text('Remove Completed')),
+                    ElevatedButton(
+                        onPressed: (() {}),
+                        child: const Text('Mark All Completed'))
+                  ],
+                ),
+              ],
+            ),
+            const Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  "add a todo",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                )),
+          ],
         ),
       ),
     );
